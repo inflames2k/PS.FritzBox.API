53b01ab7a9f0bf0c9ca2f0981cbdf781aa27cb5b
inflames2k <inflames2k@web.de>
2018-06-14 20:49:19 +0200

    Device search by UPnP added. Current solution is using SsdpRadar. Maybe future versions of the DeviceLocator will use an own implementation.

---
+++ PS.FritzBox.API/DeviceLocator.cs
@@ -0,0 +1,51 @@
+﻿using SsdpRadar;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PS.FritzBox.API
+{
+    /// <summary>
+    /// class for locating upnp devices
+    /// </summary>
+    public class DeviceLocator 
+    {
+        #region Methods
+
+        /// <summary>
+        /// Method to start discovery
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<FritzDevice>> DiscoverAsync()
+        {
+            List<FritzDevice> devices = new List<FritzDevice>();
+            FinderService service = new SsdpRadar.FinderService(5, TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(2));
+            
+            var response = await service.FindDevicesAsync();
+            foreach(var item in response)
+            {
+                if(item.Info != null && item.Info.Manufacturer.StartsWith("AVM")
+                   && item.Info.DeviceType.Equals("urn:schemas-upnp-org:device:InternetGatewayDevice:1"))
+                {
+                    FritzDevice device = new FritzDevice();
+                    device.DeviceType = item.Info.DeviceType;
+                    device.FriendlyName = item.Info.FriendlyName;
+                    device.Manufacturer = item.Info.Manufacturer;
+                    device.ModelName = item.Info.ModelName;
+                    device.IPAddress = item.RemoteEndPoint;
+                    device.Port = item.Location.Port;
+
+                    devices.Add(device);
+                }
+            }
+
+            return devices;
+        }
+
+        #endregion
+    }
+}
---
+++ PS.FritzBox.API/FritzDevice.cs
@@ -0,0 +1,63 @@
+﻿using PS.FritzBox.API.Base;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace PS.FritzBox.API
+{
+    /// <summary>
+    /// class representing a fritz device
+    /// </summary>
+    public class FritzDevice
+    {
+        /// <summary>
+        /// Gets the device type
+        /// </summary>
+        public string DeviceType { get; internal set; }
+
+        /// <summary>
+        /// Gets the friendly name
+        /// </summary>
+        public string FriendlyName { get; internal set; }
+
+        /// <summary>
+        /// Gets the manufacturer
+        /// </summary>
+        public string Manufacturer { get; internal set; }
+
+        /// <summary>
+        /// Gets the model name
+        /// </summary>
+        public string ModelName { get; internal set; }
+
+        /// <summary>
+        /// Gets the ip address
+        /// </summary>
+        public IPAddress IPAddress { get; internal set; }
+
+        /// <summary>
+        /// Gets the port
+        /// </summary>
+        public int Port { get; internal set; }
+
+        /// <summary>
+        /// Method to get service client
+        /// </summary>
+        /// <typeparam name="T">type param</typeparam>
+        /// <param name="settings">connection settings</param>
+        /// <returns>the service client</returns>
+        public T GetServiceClient<T>(ConnectionSettings settings)
+        {
+            if (String.IsNullOrEmpty(settings.BaseUrl))
+            {
+                settings.BaseUrl = $"http://{this.IPAddress}:{this.Port}";
+                // get the security port
+                int port = new DeviceInfoClient(settings.BaseUrl, settings.Timeout).GetSecurityPortAsync().GetAwaiter().GetResult();
+                settings.BaseUrl = $"https://{this.IPAddress}:{port}";
+            }
+
+            return (T)Activator.CreateInstance(typeof(T), settings);
+        }
+    }
+}
