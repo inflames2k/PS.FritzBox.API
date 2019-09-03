79d63a6d9759077604ad1a37cd11b532ff29106d
inflames2k <inflames2k@web.de>
2019-09-03 12:18:33 +0200

    Added WANEthernetLinkConfigClient

---
+++ PS.FritzBox.API/FritzBox/WANDevice/WANConnectionDevice/EthernetLinkStatus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS.FritzBox.API.WANDevice.WANConnectionDevice
+{
+    /// <summary>
+    /// enum for ethernetlink status 
+    /// </summary>
+    public enum EthernetLinkStatus
+    {
+        /// <summary>
+        /// link up
+        /// </summary>
+        Up,
+        /// <summary>
+        /// link down
+        /// </summary>
+        Down,
+        /// <summary>
+        /// unavailable
+        /// </summary>
+        Unavailable
+    }
+}
---
+++ PS.FritzBox.API/FritzBox/WANDevice/WANConnectionDevice/WANEthernetLinkConfigClient.cs
@@ -0,0 +1,56 @@
+﻿using PS.FritzBox.API.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PS.FritzBox.API.WANDevice.WANConnectionDevice
+{
+    /// <summary>
+    /// client for WAN Ethernet Link Config service
+    /// </summary>
+    public class WANEthernetLinkConfigClient : FritzTR64Client
+    {
+        #region Construction / Destruction
+
+        public WANEthernetLinkConfigClient(string url, int timeout) : base(url, timeout)
+        {
+        }
+
+        public WANEthernetLinkConfigClient(string url, int timeout, string username) : base(url, timeout, username)
+        {
+        }
+
+        public WANEthernetLinkConfigClient(string url, int timeout, string username, string password) : base(url, timeout, username, password)
+        {
+        }
+
+        public WANEthernetLinkConfigClient(ConnectionSettings connectionSettings) : base(connectionSettings)
+        {
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Gets the control url
+        /// </summary>
+        protected override string ControlUrl => "/upnp/control/wanethlinkconfig1";
+
+        /// <summary>
+        /// Gets the request namespace
+        /// </summary>                                 
+        protected override string RequestNameSpace => "urn:dslforum-org:service:WANEthernetLinkConfig:1";
+
+        /// <summary>
+        /// Method to get the ethernet link status
+        /// </summary>
+        /// <returns>the ethernet link status</returns>
+        public async Task<EthernetLinkStatus> GetEthernetLinkStatusAsync()
+        {
+            XDocument document = await this.InvokeAsync("GetEthernetLinkStatus", null);
+            return (EthernetLinkStatus)Enum.Parse(typeof(EthernetLinkStatus), document.Descendants("NewEthernetLinkStatus").First().Value);
+        }
+    }
+}
