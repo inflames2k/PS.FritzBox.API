444575f74a86e07a2f4b94d763575e8e1100606f
RoSchmi <Dr-Roland-Schmidt@t-online.de>
2022-01-13 20:32:49 +0100

    Added file in lib

---
+++ PS.FritzBox.API/FritzBox/LANConfigSecurityUserList.cs
@@ -0,0 +1,14 @@
+﻿namespace PS.FritzBox.API
+{
+    /// <summary>
+    /// class representing a userlist
+    /// </summary>
+    public class LANConfigSecurityUserList
+    {
+        /// <summary>
+        /// gets or sets the userlist
+        /// </summary>
+        public string UserList { get; set; }
+
+    }
+}
